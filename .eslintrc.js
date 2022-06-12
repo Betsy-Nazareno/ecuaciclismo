395ced4dfd1278377ad0a7a1b079722141c29cd7
Betsy-Nazareno <belinaza@espol.edu.ec>
2022-06-12 16:54:05 -0500

    cambios

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "react-native/react-native": true,
+    'react-native/react-native': true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "react-native", "@typescript-eslint"],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
   rules: {},
   settings: {
     react: {
-      version: "16.12.0",
+      version: '16.12.0',
     },
   },
-};
+}
--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  const varr = 'fd'
   return (
     <View style={styles.container}>
       <Text>Hi, this is the Ecuaciclismo app frontend!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
